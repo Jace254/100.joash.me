32c80b97b9d312b739195dd06b657905b8be9dce
Hypnos <joashmacer@gmail.com>
2024-01-03 00:20:21 +0300

    chore: add mail

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -6,7 +6,11 @@
       <a href="https://twitter.com/JoashMacenton" rel="noopener" target="_blank" class="link">twitter</a>
       &bull;
       <a href="https://github.com/Jace254/" rel="noopener" target="_blank" class="link">github</a>
+      &bull;
+      <a href="mailto:j@joash.me" rel="noopener" target="_blank" class="link">j@joash.me</a>
     </h1>
-    <p text-border>From 2024/01/02</p>
+    <p text-border>
+      From 2024/01/02
+    </p>
   </footer>
 </template>
