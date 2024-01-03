f51377716e47154a4d315a4068baccc641a7cf18
Hypnos <joashmacer@gmail.com>
2024-01-03 18:38:02 +0300

    chore: add day 2, setup radial menu styles

--- lib/index.ts
+++ lib/index.ts
@@ -5,6 +5,10 @@ const info: Info[] = [
     title: 'Balance',
     date: '01/02',
   },
+  {
+    title: 'Radial',
+    date: '01/03',
+  },
 ]
 
 export const submissions = info.map((i, idx) => ({ ...i, no: `${idx + 1}`.padStart(3, '0') }))
---
+++ pages/002.vue
@@ -0,0 +1,218 @@
+<script setup lang="ts">
+import { TransitionPresets, useTransition } from '@vueuse/core'
+
+definePageMeta({
+  layout: 'submission',
+})
+
+const size = ref(0.5)
+const opacity = ref(0)
+const duration = 500
+
+const output = useTransition(size, {
+  duration,
+  transition: TransitionPresets.easeInOutSine,
+})
+
+const outputOp = useTransition(opacity, {
+  duration,
+  transition: TransitionPresets.easeInOutSine,
+})
+
+
+
+const targetEl = ref<HTMLDivElement>()
+const { x, y } = useMouse({ target: targetEl })
+
+function toggle() {
+  size.value = size.value === 1 ? 0.5 : 1
+  opacity.value = opacity.value === 100 ? 0 : 100
+}
+</script>
+
+<template>
+  <button @click="toggle">
+    Transition {{ x }}, {{ y }}
+  </button>
+  <div
+    class="box " h="500px"
+    border border-border
+    rounded-lg flex items-center
+    justify-center bg-faded
+    relative overflow-hidden select-none
+  >
+    <div
+      class="box" ref="targetEl"
+      h="500px" border
+      border-border rounded-lg flex
+      items-center justify-center
+      bg-faded relative overflow-hidden
+      select-none
+    >
+      <div class="text">
+        Hold and rotate from anywhere
+      </div>
+      <div class="radial-box box" :style="`transform: scale(${output}); opacity: ${outputOp}%;`">
+        <div class="box radial-container" />
+        <div class="box radial-circle">
+          <span class="vh">Hold and rotate</span>
+        </div>
+        <ul class="menu">
+          <li>
+            <i i-solar-rewind-back-broken absolute text="24px" class="box item" />
+          </li>
+          <li>
+            <div i-solar-rewind-back-broken absolute h="24px" w="24px" class="box item" />
+          </li>
+          <li>
+            <div i-solar-rewind-back-broken absolute h="24px" w="24px" class="box item" />
+          </li>
+          <li>
+            <div i-solar-rewind-back-broken absolute h="24px" w="24px" class="box item" />
+          </li>
+          <li>
+            <div i-solar-rewind-back-broken absolute h="24px" w="24px" class="box item" />
+          </li>
+          <li>
+            <div i-solar-rewind-back-broken absolute h="24px" w="24px" class="box item" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <Note />
+</template>
+
+<style scoped>
+.box {
+    box-sizing: border-box;
+}
+.text {
+    position: absolute;
+    bottom: 12px;
+    left: 50%;
+    height: fit-content;
+    width: 100%;
+    text-align: center;
+    pointer-events: none;
+    transform: translate(-50%, -50%);
+    transition: opacity 250ms ease 0s;
+    top: 50%;
+}
+
+.radial-box {
+    --a: 0deg;
+    --o: 0;
+    pointer-events: auto;
+    width: 250px;
+    height: 250px;
+    border-radius: 9999px;
+    position: relative;
+    border: 1px solid hsl(0 0% 17.9%);
+    box-shadow: 0 8px 30px rgba(0, 0, 0, 0.12);
+    z-index: 5;
+    touch-action: none;
+}
+
+.radial-container {
+    content: "";
+    position: absolute;
+    border: 8px solid hsl(0 0% 13.6%);
+    width: calc(100% + 24px);
+    height: calc(100% + 24px);
+    border-radius: inherit;
+    z-index: -1;
+    inset: -12px;
+}
+
+.radial-circle {
+    width: 125px;
+    height: 125px;
+    border-radius: 9999px;
+    background:hsl(0 0% 11.0%);
+    position: absolute;
+    border: 1px solid hsl(0 0% 17.9%);
+    top: 50%;
+    z-index: 3;
+    transform: translate(-50%, -50%);
+    left: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.vh {
+    clip: rect(0 0 0 0);
+    /* -webkit-clip-path: inset(50%);
+    clip-path: inset(50%); */
+    height: 100%;
+    font-size: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    --at-apply: 'text-muted-foreground';
+    overflow: hidden;
+    white-space: nowrap;
+    width: 100%;
+}
+
+.menu {
+    width: 100%;
+    height: 100%;
+    border-radius: inherit;
+    overflow: hidden;
+    position: relative;
+    box-shadow: 0 0 0 4px var(--colors-gray2);
+}
+
+li {
+    display: list-item;
+    text-align: -webkit-match-parent;
+}
+
+.menu li {
+    --a: 30deg;
+    position: absolute;
+    bottom: 50%;
+    right: 50%;
+    font-size: 24px;
+    width: 240px;
+    height: 240px;
+    transform-origin: 100% 100%;
+    overflow: hidden;
+    background: hsl(0 0% 13.6%);
+    box-shadow: 0 0 0 1px hsl(0 0% 17.9%);
+    color: hsl(0 0% 62.8%);
+    transition: background 250ms ease 0s;
+}
+
+.menu li:nth-of-type(1) {
+    transform: rotate(calc(var(--a) * -1)) skew(var(--a));
+}
+.menu li:nth-of-type(2) {
+    transform: rotate(var(--a)) skew(var(--a));
+}
+.menu li:nth-of-type(3) {
+    transform: rotate(calc(var(--a) * 3)) skew(var(--a));
+}
+.menu li:nth-of-type(4) {
+    transform: rotate(calc(var(--a) * 5)) skew(var(--a));
+}
+.menu li:nth-of-type(5) {
+    transform: rotate(calc(var(--a) * 7)) skew(var(--a));
+}
+.menu li:nth-of-type(6) {
+    transform: rotate(calc(var(--a) * 9)) skew(var(--a));
+}
+
+li:active {
+    background: hsl(0 0% 17.9%);
+    color: hsl(0 0% 93.0%);
+}
+
+.item {
+    transform: skew(calc(var(--a) * -1)) rotate(var(--a)) translateX(330px) translateY(15px);
+	font-size: 1.5rem;
+}
+</style>
