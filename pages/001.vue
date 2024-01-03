11e2af7b50d332d85877eebd6527f15ac4a17838
Hypnos <joashmacer@gmail.com>
2024-01-03 09:32:48 +0300

    debug

--- pages/001.vue
+++ pages/001.vue
@@ -9,6 +9,7 @@ definePageMeta({
 
 const route = useRoute()
 
+const debug = !!route.query.debug
 const shot = !!route.query.shot
 const { gamma, beta, alpha } = useDeviceOrientation()
 
@@ -38,14 +39,23 @@ const transformStyle = computed(() => {
     <div rounded-t-full border border-b-0 border-foreground w-9em h-5em flex justify-center items-center>
       o o<br>
     </div>
+    <div fixed left="50%" top="50%" w-0 h-0 overflow-visible>
+      <a class="link" href="https://en.wikipedia.org/wiki/Kokeshi" target="_blank" rel="noopener" text-center style="width: 500px;transform: translate(-50%, -50%);">Kokeshi</a>
+    </div>
     <div rounded-b-full border border-foreground w-9em h-9em flex justify-center items-center>
-      <a class="link" href="https://en.wikipedia.org/wiki/Kokeshi" target="_blank" rel="noopener">Kokeshi</a>
+      ( )
     </div>
   </div>
   <div py3 class="box-description">
     <p v-if="!mobile && !shot" text-gray text-center>
       Use mobile to visit
     </p>
+  </div>
+  <div v-if="debug" p4>
+    <p>Gamma {{ gamma }}</p>
+    <p>Beta  {{ beta }}</p>
+    <p>Theta {{ theta }}</p>
+    <p>Alpha {{ alpha }}</p>
   </div>
 
   <Note>
